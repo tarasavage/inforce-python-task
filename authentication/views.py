6677cc4921e5be3419d5e70bdda1c25b50e472a5
Taras Panasiuk <107722931+tarasavage@users.noreply.github.com>
2023-07-03 14:24:22 +0300

    Added user registration with password confirmation

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from authentication.serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
