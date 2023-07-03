de0939ec29271944a02ea0946f4e767601050c7b
Taras Panasiuk <107722931+tarasavage@users.noreply.github.com>
2023-07-03 13:40:05 +0300

    Added app authentication

--- restaurant_api/settings.py
+++ restaurant_api/settings.py
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "authentication",
 ]
+
+AUTH_USER_MODEL = "authentication.User"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
